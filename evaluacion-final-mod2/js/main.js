2cf837a3add73cadf3690073854863ae29c7f124
aminamrc <amina.mrc@gmail.com>
2024-01-20 21:25:45 +0100

    img placehold

--- evaluacion-final-mod2/js/main.js
+++ evaluacion-final-mod2/js/main.js
@@ -5,7 +5,7 @@ const inputAnime = document.querySelector('.js-inputAnime');
 const btn= document.querySelector('.js-btn');
 const reset= document.querySelector('.js-reset');
 const resultsContainer=document.querySelector('.js-results');
-const animeList=document.querySelector('.js-anime-list');
+let animeList=document.querySelector('.js-anime-list');
 const favoritesContainer=document.querySelector('.js-favorites');
 const favoritesList=document.querySelector('.js-favorite-list');
 const inputAnimeValue=inputAnime.value;
@@ -19,8 +19,9 @@ function getDataApi () {
     .then (response => response.json())
     .then (data => {
       dataAnime=data.data;
-      console.log(dataAnime)
+      console.log(dataAnime);
       renderAnime (dataAnime,resultsContainer);
+      localStorage.setItem('anime', JSON.stringify(animeList));
     }); 
 }
 
@@ -37,6 +38,9 @@ function renderAnime (array,container) {
     // console.log(allimgAnime.jpg.image_url); 
     animeList += `<li class="list_anime js-anime-selected" id="${each.mal_id}"> <p> ${each.title} </p>  
      <img src="  ${each.images.jpg.image_url}" alt="img" > </img> </li>`
+     if (each.images.jpg.image_url === 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png') {
+        each.images.jpg.image_url ='https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+     } else (`${each.images.jpg.image_url}`);
     } 
     container.innerHTML = animeList;
     listenerAnimeList ();
@@ -89,17 +93,17 @@ function listenerAnimeList () {
      }
 
 
-    //  function getDataLocalStorage() {
-    //     const dataAnimeLocal = JSON.parse(localStorage.getItem('anime'));
-    //     if (dataAnimeLocal !== null) {
-    //       animeList = dataAnimeLocal;
-    //       renderAnime(dataAnimeLocal, container);
-    //     } else {
-    //       getDataApi();
-    //     }
-    //     console.log(animeList);
-    //   }
-    //   getDataLocalStorage();
+     function getDataLocalStorage() {
+        const dataAnimeLocal = JSON.parse(localStorage.getItem('anime'));
+        if (dataAnimeLocal !== null) {
+          animeList = dataAnimeLocal;
+          renderAnime(dataAnimeLocal, resultsContainer);
+        } else {
+          getDataApi();
+        }
+        console.log(animeList);
+      }
+      getDataLocalStorage();
 
      
      // animeList[one].addEventListener('click', addFavorite);
